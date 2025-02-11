6421aafb91c67e5a6a27183d9829e644fe26361e
gulshanpr <gulshankumarprasad04@gmail.com>
2025-02-11 13:22:28 +0530

    added all sorting and filtering

--- src/data/filterOptions.js
+++ src/data/filterOptions.js
@@ -24,27 +24,23 @@ export const statuses = [
 ];
 
 export const fundingTopics = [
-  { value: "AI & Web3", label: "AI & Web3" },
-  { value: "AI & ZK (Hybrid)", label: "AI & ZK (Hybrid)" },
-  { value: "Cross-Chain & Interoperability", label: "Cross-Chain & Interoperability" },
-  { value: "DAOs & Governance", label: "DAOs & Governance" },
+  { value: "AI", label: "AI" },
+  { value: "AIAgents", label: "AIAgents" },
+  { value: "CrossChain", label: "CrossChain" },
+  { value: "DAOs", label: "DAOs" },
   { value: "Data & Oracles", label: "Data & Oracles" },
   { value: "DeFi", label: "DeFi" },
-  { value: "DePIN (Decentralized Physical Infrastructure)", label: "DePIN (Decentralized Physical Infrastructure)" },
+  { value: "DePIN", label: "DePIN" },
   { value: "Developer Tooling", label: "Developer Tooling" },
   { value: "Education", label: "Education" },
   { value: "Gaming & Metaverse", label: "Gaming & Metaverse" },
-  { value: "Hardware & IoT", label: "Hardware & IoT" },
   { value: "Infrastructure", label: "Infrastructure" },
-  { value: "MEV & Transaction Optimization", label: "MEV & Transaction Optimization" },
   { value: "NFTs & Creator Economy", label: "NFTs & Creator Economy" },
   { value: "Privacy & Security", label: "Privacy & Security" },
   { value: "Public Goods & Open Source", label: "Public Goods & Open Source" },
-  { value: "Real World Assets (RWA)", label: "Real World Assets (RWA)" },
-  { value: "Regenerative Finance (ReFi)", label: "Regenerative Finance (ReFi)" },
   { value: "Social & Community", label: "Social & Community" },
   { value: "Stablecoins & Payments", label: "Stablecoins & Payments" },
-  { value: "Zero Knowledge (ZK)", label: "Zero Knowledge (ZK)" }
+  { value: "Zero Knowledge", label: "Zero Knowledge (ZK)" }
 ];
 
 export const fundingTypes = [
