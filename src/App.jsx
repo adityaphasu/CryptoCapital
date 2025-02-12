33fb5b8c21da7e79348d26ccd5a1f7f35845fb62
Aditya Phasu <adityaphasu919@gmail.com>
2025-02-12 22:28:08 +0530

    Add favicon and logo images; update header and footer content

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
       <Header />
       <main className="flex flex-col items-center justify-center pt-40 lg:pt-32 pb-5">
         <div className="flex flex-col items-center space-y-4">
-          <h1 className="text-4xl font-medium text-center">CL Web3 Grants Database</h1>
-          <p className="text-[#ffffff7a] max-w-[45rem] text-center px-4">
+          <h1 className="text-4xl font-medium text-center">CL Web3 Grants Dashboard</h1>
+          <p className="text-[#ffffff7a] max-w-[47rem] text-center px-4">
             Empowering Web3 projects with a dynamic grant database of over 80 opportunities.
             <br />
             The database is updated every 48 hours to ensure the most accurate and active
@@ -18,7 +18,7 @@ function App() {
         <div className="w-[80%] mx-auto my-6">
           <div className="flex items-center pt-3 gap-5 font-medium text-white mb-0.5 border-b border-gray-600">
             <p className="relative">
-              Search Grants
+              Search Grants Database
               <span className="w-full absolute bg-[#00bbfc] h-0.5 left-0 bottom-0"></span>
             </p>
           </div>
@@ -26,10 +26,30 @@ function App() {
         </div>
       </main>
       <footer className="flex flex-col items-center space-y-2 pb-8">
-        <div className="w-[14.625rem] mb-1">
+        <div className="flex flex-col justify-center items-center mb-1 gap-2">
           <a href="https://www.cornarolabs.xyz" target="_blank" rel="noreferrer">
-            <img src="./logo.png" alt="" />
+            <img src="./logo.png" alt="" className="w-[14.625rem]" />
           </a>
+          <div>
+            <p className="text-white text-sm text-center">
+              CL Web3 Grants Dashboard by{" "}
+              <a
+                href="http://www.cornarolabs.xyz"
+                target="_blank"
+                className="text-blue-500 hover:underline">
+                Cornaro Labs
+              </a>{" "}
+              is built in collaboration with:
+            </p>
+            <div className="flex gap-2 items-center px-2">
+              <a href="https://www.blackvogel.com/" target="_blank" rel="noreferrer">
+                <img src="./logo2.svg" alt="" className="w-52" />
+              </a>
+              <a href="https://hbi.gr/en/" target="_blank" rel="noreferrer">
+                <img src="./logo3.jpeg" alt="" className="w-52" />
+              </a>
+            </div>
+          </div>
         </div>
         <div>
           <p className="flex flex-col items-center space-y-2 mt-4 text-[#ffffff7a]/20 text-xs text-center">
