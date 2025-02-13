8c78c9a680f85672d7fb781e4f5ecf1905b1f20b
DarthPrajal <prajalsharma1120@gmail.com>
2025-02-13 13:31:06 +0530

    Update GrantCard.jsx

--- src/components/GrantCard.jsx
+++ src/components/GrantCard.jsx
@@ -56,7 +56,7 @@ const GrantCard = ({ grant }) => {
               )}
             </Button>
             <a
-              href={grant.website}
+              href={grant.website} target="_blank"
               className="bg-[#00bbfc] text-white px-4 py-2 rounded-md text-sm font-semibold hover:bg-sky-600 transition-colors">
               Apply
             </a>
